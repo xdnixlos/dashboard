7153bf0d0d0c97e277213cb6e7dbf7a9ceca15bd
xdnixlos <admin@wf-tech.de>
2025-06-29 16:17:45 +0200

    Final Release - PWA

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'wf-dashboard-cache-v1';
+const urlsToCache = [
+  '/',
+  '/views/index.html',
+  '/images/icon-192.png', // Icon zum Cache hinzufügen
+  '/images/icon-512.png', // Icon zum Cache hinzufügen
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
